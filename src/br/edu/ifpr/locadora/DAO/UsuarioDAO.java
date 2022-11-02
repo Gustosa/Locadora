9da8126107787b27329dcd8e875567332f4fa1f3
Gustosa <107715611+Gustosa@users.noreply.github.com>
2022-11-02 14:58:48 -0300

    Add files via upload

---
+++ src/br/edu/ifpr/locadora/DAO/UsuarioDAO.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.locadora.DAO;
+
+import br.edu.ifpr.locadora.entities.Usuario;
+import br.edu.ifpr.locadora.factories.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fabri
+ */
+public class UsuarioDAO {
+    public void cadastrarUsuario(Usuario usuario) throws SQLException{
+        String sql = "INSERT INTO USUARIO (LOGIN, NOME, SENHA, ISADM) VALUES (?, ?, ?, ?)";
+        
+        Connection con = new ConnectionFactory().getConnection();
+        
+        PreparedStatement stmt = con.prepareStatement(sql);
+        
+        stmt.setString(1, usuario.getLogin());
+        stmt.setString(2, usuario.getNome());
+        stmt.setString(3, usuario.getSenha());
+        stmt.setBoolean(4, usuario.getAdm());
+        
+        stmt.execute();
+        
+        stmt.close();
+        con.close();
+    }
+    
+    public ArrayList<Usuario> selecionarUsuario() throws SQLException{
+        ArrayList<Usuario> retorno = new ArrayList<>();
+        String sql = "SELECT LOGIN, NOME, SENHA ORDER BY NOME";
+        
+        Connection con = new ConnectionFactory().getConnection();
+        
+        PreparedStatement stmt = con.prepareStatement(sql);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+        while(rs.next() == true){
+            Usuario u = new Usuario();
+            u.setLogin(rs.getString("LOGIN"));
+            u.setNome(rs.getString("NOME"));
+            u.setSenha(rs.getString("SENHA"));
+            
+            retorno.add(u);
+        }
+        
+        return retorno;
+    }
+    
+    public void alterarUsuario(Usuario u) throws SQLException{
+        String sql = "UPDATE USUARIO SET LOGIN = ?, NOME = ?, SENHA = ? WHERE LOGIN = ?";
+        
+        Connection con = new ConnectionFactory().getConnection();
+        
+        PreparedStatement stmt = con.prepareStatement(sql);
+        
+        stmt.setString(1, u.getLogin());
+        stmt.setString(2, u.getNome());
+        stmt.setString(3, u.getSenha());
+        stmt.setString(4, u.getLogin());
+        
+        stmt.execute();
+        
+        stmt.close();
+        con.close();
+    }
+    
+    public void removerUsuario(Usuario u) throws SQLException{
+        String sql = "DELETE FROM USUARIO WHERE LOGIN = ?";
+        
+        Connection con = new ConnectionFactory().getConnection();
+        
+        PreparedStatement stmt = con.prepareStatement(sql);
+        
+        stmt.setString(1, u.getLogin());
+        
+        stmt.execute();
+        
+        stmt.close();
+        con.close();
+    }
+}
+
+
---
+++ src/br/edu/ifpr/locadora/entities/Filme.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.locadora.entities;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author fabri
+ */
+public class Filme {
+    
+    int id;
+    String nome;
+    Date data_lancamento;
+    String genero;
+    int avaliacao;
+
+     public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getData_lancamento() {
+        return data_lancamento;
+    }
+
+    public void setData_lancamento(Date data_lancamento) {
+        this.data_lancamento = data_lancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+}
